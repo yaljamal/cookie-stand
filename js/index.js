d77a2c171fc2e9c9fbb4d1522f4956f07092fd5f
yaljamal <yazan.aljamal1@gmail.com>
2020-02-26 23:50:23 +0200

    add

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 import { color } from "ansi-styles";
 import { red } from "color-name";
 
-var sales = document.getElementById('sales');
+var sales = document.getElementById('ul');
 sales.addEventListener('change', function(event){
 event.target.style.color= "red";
 });
